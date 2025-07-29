705beaf097e3572085ea6f00d7eff1a61ef69936
hulxv <hulxxv@gmail.com>
2025-07-29 19:24:23 +0300

    remove red bg

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -6,7 +6,7 @@ export default function Login() {
         <Flex justify={"center"} w="full" h={"40vw"} align={"center"}>
             <Box maxW={"720px"} w="full" h="fit-content">
                 <Card>
-                    <Stack w="full" px={16} bg="red" align={"center"} justify={"start"}>
+                    <Stack w="full" px={16} align={"center"} justify={"start"}>
                         <Heading>Register</Heading>
                     </Stack>
                 </Card>
