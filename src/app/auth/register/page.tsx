2a5782c71a2348e391bdca330b5fcdf8dcb80e1e
hulxv <hulxxv@gmail.com>
2025-07-29 19:22:59 +0300

    add initial pages

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,16 @@
+import Card from "@/components/ui/internal/card";
+import { Box, Flex, Heading, Stack } from "@chakra-ui/react";
+
+export default function Login() {
+    return (
+        <Flex justify={"center"} w="full" h={"40vw"} align={"center"}>
+            <Box maxW={"720px"} w="full" h="fit-content">
+                <Card>
+                    <Stack w="full" px={16} bg="red" align={"center"} justify={"start"}>
+                        <Heading>Register</Heading>
+                    </Stack>
+                </Card>
+            </Box>
+        </Flex>
+    );
+}
