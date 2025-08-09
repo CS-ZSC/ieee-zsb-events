d01c02ce93b08a127732b1d266188d2c95d17a6e
hulxv <hulxxv@gmail.com>
2025-08-09 19:25:40 +0300

    improve hero section

--- src/components/ui/internal/hero.tsx
+++ src/components/ui/internal/hero.tsx
@@ -12,7 +12,7 @@ export function Hero() {
         <Flex direction={isDesktop ? "row-reverse" : "column"} align={"center"} justify={"center"} w="full" spaceX={isDesktop ? 20 : 0} padding={isDesktop ? 10 : 2}>
             <LogoHorse type="blue" height={isDesktop ? 820 : 400} width={isDesktop ? 820 : 320} />
             <Stack align={isDesktop ? "start" : "center"} justify={"center"} spaceY={4} maxW={isDesktop ? "500px" : "full"}>
-                <Heading size="3xl">
+                <Heading fontSize={isDesktop ? "2rem": "1.8rem"} textAlign={"center"} fontWeight={"bold"}>
                     Events and Competitions gate
                 </Heading>
                 <Stack textAlign={isDesktop ? "start" : "center"} maxW={isDesktop ? "500px" : "full"} spaceY={-2} fontSize={"lg"}>
